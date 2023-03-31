7bffa010129181d48b9c03f4560ffa01ebdb5695
RoofMi <overstar_@naver.com>
2023-04-01 03:35:27 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,32 +1,83 @@
 let scrollTop = 0;
 let bar;
 
-window.onload = function () {
-  bar = document.getElementsByClassName("scroll_bar_ing")[0];
-};
+$(document).ready(function () {
+  bar = $(".scroll_bar_ing")[0];
+});
 
-window.addEventListener( "scroll", () => {
-  scrollTop = document.documentElement.scrollTop; // y축 방향으로 얼만큼 스크롤했는지!
-  let per = Math.ceil((scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100);
-    bar.style.width = per + "%";
-  },
-  false
-);
+$(window).scroll(function () {
+  scrollTop = $(document).scrollTop();
+  let per = Math.ceil((scrollTop / ($(document.body).outerHeight() - $(window).outerHeight())) * 100);
+  $(bar).css("width", per + "%");
+});
 
-$(function(){
-  var $header = $('header'); //헤더를 변수에 넣기
+$(function () {
+  var $header = $('header');
   var $scroll_bar_down = $('.scroll_bar');
-  var $page = $('#section_about'); //색상이 변할 부분
+  var $page = $('#section_about');
   var $window = $(window);
-  var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
-  
-  $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+  var pageOffsetTop = $page.offset().top;
+
+  $window.resize(function () {
     pageOffsetTop = $page.offset().top;
   });
 
-  $window.on('scroll', function(){ //스크롤시
-    var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
-    $header.toggleClass('header_down', scrolled); //클래스 토글
-    $scroll_bar_down.toggleClass('scroll_bar_down', scrolled); //클래스 토글
+  $window.scroll(function () {
+    var scrolled = $window.scrollTop() >= pageOffsetTop;
+    $header.toggleClass('header_down', scrolled);
+    $scroll_bar_down.toggleClass('scroll_bar_down', scrolled);
+  });
+
+  $scroll_bar_down.on('mousedown', function (e) {
+    e.preventDefault();
+    var y = e.clientY;
+    $(document).on('mousemove', function (e) {
+      var newY = e.clientY;
+      var distance = newY - y;
+      var scrollPosition = $window.scrollTop();
+      $window.scrollTop(scrollPosition - distance);
+      y = newY;
+    });
+  });
+
+  $(document).on('mouseup', function () {
+    $(document).off('mousemove');
   });
 });
+
+var spanText = function spanText(text) {
+  var lines = $(text).html().split("<br>");
+  var spaned = "";
+  for (var i = 0; i < lines.length; i++) {
+    var string = lines[i];
+    var lineSpaned = "";
+    for (var j = 0; j < string.length; j++) {
+      if (string.substring(j, j + 1) === " ") {
+        lineSpaned += string.substring(j, j + 1);
+      } else {
+        lineSpaned += '<span>' + string.substring(j, j + 1) + '</span>';
+      }
+    }
+    spaned += lineSpaned + "<br>";
+  }
+  $(text).html(spaned);
+};
+
+$(document).ready(function () {
+  var headline = $(".home_text")[0];
+  spanText(headline);
+
+  let animations = $('.home_text');
+
+  animations.each(function (i, animation) {
+    let letters = $(animation).find('span');
+    letters.each(function (i, letter) {
+      $(letter).css("animation-delay", (i * 0.1) + 's');
+    })
+  })
+});
+
+function scrollToSection(selector) {
+  const section = $(selector)[0];
+  $(section).get(0).scrollIntoView({ behavior: 'smooth' });
+}
